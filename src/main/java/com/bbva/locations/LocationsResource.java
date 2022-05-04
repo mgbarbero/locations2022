a59d7f087fb18a45eafa6747f4198e766e1e416c
mgbarbero <mgbarbero@gmail.com>
2022-05-04 16:14:28 -0300

    MASTER

---
+++ src/main/java/com/bbva/locations/LocationsResource.java
@@ -0,0 +1,21 @@
+package com.bbva.locations;
+
+import com.bbva.locations.beans.Location;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * A JAX-RS interface.  An implementation of this interface must be provided.
+ */
+@Path("/locations")
+public interface LocationsResource {
+  @GET
+  @Produces("application/json")
+  List<Location> generatedMethod1();
+
+  @POST
+  void generatedMethod2();
+}
